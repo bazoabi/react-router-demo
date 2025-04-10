9d7dbd410bb361e151ad2f5697a9210c422606dc
Bader Zoabi <bader.zoabi@gmail.com>
2025-04-10 07:31:00 +0200

    Create dedicated page to show the details of each post, called PostDetails, and apply the proper redirection inclduing providing postId as url param

--- src/PostsList.js
+++ src/PostsList.js
@@ -1,16 +1,30 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { postsContext } from "./contexts/postsContext";
 
 export default function PostsList() {
+  const posts = useContext(postsContext);
+  const postsListRenderer = posts.map((post) => {
+    return (
+      <Link key={post.id} to={`/postDetails/${post.id}`}>
+        <div style={{ background: "orange", padding: "1vh", margin: "3vh" }}>
+          <h1>{post.title}</h1>
+          <hr />
+          {/* <h4>{post.body}</h4> */}
+        </div>
+      </Link>
+    );
+  });
+
   return (
     <>
-      <h1>Post Title:....</h1>
-      <p>lorem ipsum.....</p>
+      {postsListRenderer}
       <Link to="/home">
         <div
           style={{
             background: "orange",
             padding: "10px",
-            borderRadius: "10px",
+            borderRadius: "40px",
           }}
         >
           <h1>Go To HomePage</h1>
